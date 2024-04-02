9e16ec674451ae9145add9d85da8202c2faf819f
Afrie Irham <afrieirham.work@gmail.com>
2024-04-02 23:50:37 +0800

    extract JobPosting metadata from html

--- scraper/scripts/src/fetch.ts
+++ scraper/scripts/src/fetch.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import qs from "query-string";
+import { load } from "cheerio";
 import { format, sub } from "date-fns";
+import qs from "query-string";
 
 import { title } from "./constant";
-import { GoogleAPIResult, Item } from "./types";
+import { GoogleAPIResult } from "./types";
 
 const runner = async () => {
   // start construct google query
@@ -17,7 +18,7 @@ const runner = async () => {
   const key = process.env.GOOGLE_SEARCH_KEY;
   const URL = "https://www.googleapis.com/customsearch/v1";
 
-  let allResults: Item[] = [];
+  let links = [];
   let start = 1;
 
   while (true) {
@@ -25,8 +26,6 @@ const runner = async () => {
       url: URL,
       query: { start, cx, key, q: query },
     });
-
-    console.log(requestUrl);
 
     const { data } = await axios.get(requestUrl);
     const results = data as GoogleAPIResult;
@@ -38,7 +37,7 @@ const runner = async () => {
 
     // add results if exist
     if (results.items.length > 0) {
-      allResults.push(...results.items);
+      links.push(...results.items.map(({ link }) => link));
     }
 
     // stop if no next page
@@ -52,8 +51,47 @@ const runner = async () => {
       break;
     }
   }
+
+  if (links.length === 0) {
+    return;
+  }
   // end fetching jobs
-  console.log(allResults);
+
+  // start get html from links
+  const responses = await Promise.all(links.map((link) => fetch(link)));
+  const success = responses.filter((res) => res.status < 400);
+  const htmls = await Promise.all(success.map((res) => res.text()));
+  // end get html from links
+
+  // start extract json-ld from htmls
+  const jobs = htmls.map((html) => {
+    // get all ld+json
+    const $ = load(html);
+    let jobSchema = $("script[type='application/ld+json']").text();
+    if (!jobSchema) {
+      return false;
+    }
+
+    // start if there's multiple ld+json
+    const split = jobSchema.split("}{");
+    if (split.length > 1) {
+      const fixed = split.map((item, i) =>
+        i % 2 === 0 ? item + "}" : "{" + item
+      );
+      const formatted = fixed.map((i) => JSON.parse(i));
+      const schema = formatted.find((item) => item["@type"] === "JobPosting");
+      return schema;
+    }
+    // end if there's multiple ld+json
+
+    const parsedSchema = JSON.parse(jobSchema);
+    if (parsedSchema && parsedSchema["@type"] === "JobPosting") {
+      return parsedSchema;
+    }
+    return false;
+  });
+  // end extract json-ld from htmls
+  console.log(jobs);
 };
 
 runner();
