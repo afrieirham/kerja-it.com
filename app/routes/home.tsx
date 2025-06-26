dae02a0dbc51373862b0f0a22863b2c427d1f3a0
Afrie Irham <afrieirham.work@gmail.com>
2025-06-26 17:39:35 +0800

    setup prisma and t3 env

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
+import { db } from "server/db";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +8,22 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function loader({ params }: Route.LoaderArgs) {
+  return await db.job.findMany();
+}
+
+export default function Home(props: Route.ComponentProps) {
+  const jobs = props.loaderData;
+  console.log(jobs);
+  return (
+    <main className="mx-auto max-w-6xl p-4">
+      <h1 className="text-xl">Kerja-IT.com</h1>
+      <p className="text-sm">
+        IT jobs in Malaysia sourced from various job boards.{" "}
+        <span className="text-xs text-gray-500">
+          (yes new simplified design)
+        </span>
+      </p>
+    </main>
+  );
 }
