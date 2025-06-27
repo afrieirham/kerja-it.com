67c8a7d40003794fa778e240749285e369c808b8
Afrie Irham <afrieirham.work@gmail.com>
2025-06-27 18:30:10 +0800

    remove trpc

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,5 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("home/home.tsx"),
   route("api/cron", "api/cron-insert.ts"),
-  route("api/trpc/*", "api/trpc.ts"),
 ] satisfies RouteConfig;
