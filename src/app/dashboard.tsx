ea24f1b7a8258ccc3c4b94d6789fa1033e09b93b
Afrie Irham <afrieirham.work@gmail.com>
2025-07-01 13:48:36 +0800

    use stripe sdk to create checkout session and remove stripe pricing table

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,10 +1,26 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
+import { redirect, useFetcher } from "react-router";
+
 import { getAuth } from "@clerk/react-router/ssr.server";
-import { redirect } from "react-router";
+import type { VariantProps } from "class-variance-authority";
+import Stripe from "stripe";
 
 import { Header } from "@/components/header";
+import { Button, type buttonVariants } from "@/components/ui/button";
+import { env } from "@/env";
 import { clerkClient } from "@/server/auth";
 import { db } from "@/server/db";
 import type { Route } from "./+types/dashboard";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Kerja-IT.com | Dashboard" },
+    {
+      name: "description",
+      content: "IT jobs in Malaysia sourced from various job boards.",
+    },
+  ];
+}
 
 export async function loader(args: Route.LoaderArgs) {
   const clerk = await getAuth(args);
@@ -32,65 +48,146 @@ export async function loader(args: Route.LoaderArgs) {
     }
     return { credit, email };
   }
+
   return { credit: user.jobCredit, email: user.email };
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+
+  const formData = await request.formData();
+  const priceId = formData.get("priceId")?.toString();
+  const email = formData.get("email")?.toString();
+
+  if (!email || !priceId) {
+    console.log("email and priceId is required");
+    return;
+  }
+
+  const { url } = await stripe.checkout.sessions.create({
+    mode: "payment",
+    cancel_url: "http://localhost:5173/dashboard",
+    success_url: "http://localhost:5173/dashboard",
+    line_items: [{ price: priceId, quantity: 1 }],
+    customer_email: email,
+    customer_creation: "always",
+  });
+
+  if (!url) {
+    console.log("cannot create stripe checkout session");
+    return;
+  }
+
+  return redirect(url);
 }
 
 export default function Dashboard({ loaderData }: Route.ComponentProps) {
   const { credit, email } = loaderData;
+  const {
+    VITE_STRIPE_PRICE_A_ID,
+    VITE_STRIPE_PRICE_B_ID,
+    VITE_STRIPE_PRICE_C_ID,
+  } = import.meta.env;
 
   return (
     <div className="mx-auto max-w-6xl p-4">
       <Header />
       <div className="mt-8 space-y-8">
         <div>
-          <p className="text-sm font-bold">Recruiter Dashboard</p>
-          <div className="mt-2 max-w-md border bg-gray-50 p-4 text-sm">
-            <p className="font-bold">How to post a job?</p>
-            <p className="mt-2 text-sm">
-              You can either post a free listing or premium listing. Premium
-              listing requires at least 1 credit.
-            </p>
-            <p className="my-6">You have {credit} job listing credits.</p>
-            <ul className="list-inside list-disc">
-              <li>Credit can be used anytime.</li>
-              <li>All job listing will be deleted after 6 months.</li>
-              <li>Premium jobs will be featured for 30 days.</li>
-            </ul>
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-bold">Purchase Job Credits</p>
+          </div>
+          <div className="mt-2 mb-16 w-full border bg-gray-50 p-4">
+            <StripeButton
+              email={email}
+              priceId={VITE_STRIPE_PRICE_A_ID}
+              buttonText="Buy 1 Credit"
+              buttonProps={{ variant: "minimal", size: "minimal" }}
+            />
+            <StripeButton
+              email={email}
+              priceId={VITE_STRIPE_PRICE_B_ID}
+              buttonText="Buy 3 Credit"
+              buttonProps={{ variant: "minimal", size: "minimal" }}
+            />
+            <StripeButton
+              email={email}
+              priceId={VITE_STRIPE_PRICE_C_ID}
+              buttonText="Buy 5 Credit"
+              buttonProps={{ variant: "minimal", size: "minimal" }}
+            />
           </div>
         </div>
         <div>
-          <p className="text-sm font-bold">
-            Choose your preferred package below
-          </p>
-          <PricingTable
-            className="mt-2 sm:border sm:p-8"
-            pricingTableId={import.meta.env.VITE_STRIPE_PRICING_TABLE_ID}
-            publishableKey={import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY}
-            userEmail={email}
-          />
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-bold">Dashboard</p>
+            <div className="flex items-center space-x-1">
+              <Button variant="minimal" size="minimal">
+                {credit} premium credit
+              </Button>
+              <p>/</p>
+              <Button variant="minimal" size="minimal">
+                2 free posts
+              </Button>
+            </div>
+          </div>
+
+          <div className="mt-2 max-w-md space-y-4 border bg-gray-50 p-4 text-sm">
+            <div>
+              <p>How to post jobs</p>
+              <ul className="list-inside list-disc">
+                <li>
+                  You can either post a <u>free listing</u> or{" "}
+                  <u>premium listing</u>
+                </li>
+                <li>Premium listing requires at least 1 credit</li>
+                <li>All listing will be hidden after 6 months</li>
+              </ul>
+            </div>
+            <div>
+              <p>Premium listing</p>
+              <ul className="list-inside list-disc">
+                <li>Requires 1 credit per job listing</li>
+                <li>Credit can be used anytime</li>
+                <li>Premium jobs will be featured for 30 days</li>
+              </ul>
+            </div>
+            <div>
+              <p>Free listing</p>
+              <ul className="list-inside list-disc">
+                <li>Will be pinned below premium listing for 7 days</li>
+                <li>Does not require any credit</li>
+                <li>Only 2 listing per month</li>
+                <li>Must share salary info</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-function PricingTable({
-  pricingTableId,
-  publishableKey,
-  className,
-  userEmail,
+function StripeButton({
+  buttonProps,
+  email,
+  priceId,
+  buttonText,
 }: {
-  className?: string;
-  userEmail: string;
-  pricingTableId: string;
-  publishableKey: string;
+  email: string;
+  priceId: string;
+  buttonText: string;
+  buttonProps: React.ComponentProps<"button"> &
+    VariantProps<typeof buttonVariants>;
 }) {
+  const fetcher = useFetcher();
   return (
-    <div
-      className={className}
-      dangerouslySetInnerHTML={{
-        __html: `<script async src="https://js.stripe.com/v3/pricing-table.js"></script><stripe-pricing-table pricing-table-id="${pricingTableId}" publishable-key="${publishableKey}" customer-email="${userEmail}"></stripe-pricing-table>`,
-      }}
-    />
+    <fetcher.Form method="post">
+      <input type="hidden" value={email} name="email" />
+      <input type="hidden" value={priceId} name="priceId" />
+      <Button type="submit" {...buttonProps}>
+        {fetcher.state !== "idle" ? "Loading" : buttonText}
+      </Button>
+    </fetcher.Form>
   );
 }
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,8 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_CLERK_PUBLISHABLE_KEY: string;
-  readonly VITE_STRIPE_PRICING_TABLE_ID: string;
-  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
+  readonly VITE_STRIPE_PRICE_A_ID: string;
+  readonly VITE_STRIPE_PRICE_B_ID: string;
+  readonly VITE_STRIPE_PRICE_C_ID: string;
 }
 
 interface ImportMeta {
