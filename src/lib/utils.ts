a9e8f7c52f2d2fb0f6806fb5abed737b24b3ef23
Afrie Irham <afrieirham.work@gmail.com>
2025-07-02 21:57:41 +0800

    hardcode prod base url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 5173}`;
+  return "https://kerja-it.com";
 }
