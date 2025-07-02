d70d514fffb6911806285b92e5aaa2754f9d0eba
Afrie Irham <afrieirham.work@gmail.com>
2025-07-02 21:49:03 +0800

    fix server only components called by client

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,8 +4,8 @@ export default [
   index("home.tsx"),
   route("dashboard", "dashboard/dashboard.tsx"),
   route("dashboard/:jobId/edit", "dashboard/edit-job.tsx"),
-  route("api/cron/save-jobs", ".server/cron/save-jobs.ts"),
-  route("api/cron/free-credit", ".server/cron/reset-free-credit.ts"),
-  route("api/stripe/webhook", ".server/stripe/stripe-webhook.ts"),
-  route("api/stripe/checkout", ".server/stripe/create-checkout-session.ts"),
+  route("api/cron/save-jobs", "api/cron/save-jobs.ts"),
+  route("api/cron/free-credit", "api/cron/reset-free-credit.ts"),
+  route("api/stripe/webhook", "api/stripe/stripe-webhook.ts"),
+  route("api/stripe/checkout", "api/stripe/create-checkout-session.ts"),
 ] satisfies RouteConfig;
