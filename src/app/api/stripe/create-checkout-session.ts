8bae42ba194e6b659cadfbcff36f96a067a02ddb
Afrie Irham <afrieirham.work@gmail.com>
2025-07-02 21:55:23 +0800

    fix hardcoded baseUrl

--- src/app/api/stripe/create-checkout-session.ts
+++ src/app/api/stripe/create-checkout-session.ts
@@ -3,6 +3,7 @@ import Stripe from "stripe";
 
 import type { Route } from "./+types/create-checkout-session";
 import { env } from "@/env";
+import { getBaseUrl } from "@/lib/utils";
 
 export function loader(_: Route.LoaderArgs) {
   return Response.json({ message: "nothing to see here" });
@@ -12,6 +13,8 @@ export async function action({ request }: Route.ActionArgs) {
   if (request.method !== "POST") {
     return data({ message: "method not allowed" }, { status: 405 });
   }
+
+  const baseUrl = getBaseUrl();
 
   const formData = await request.formData();
 
@@ -26,8 +29,8 @@ export async function action({ request }: Route.ActionArgs) {
   const stripe = new Stripe(env.STRIPE_SECRET_KEY);
   const { url } = await stripe.checkout.sessions.create({
     mode: "payment",
-    cancel_url: "http://localhost:5173/dashboard",
-    success_url: "http://localhost:5173/dashboard",
+    cancel_url: `${baseUrl}/dashboard`,
+    success_url: `${baseUrl}/dashboard`,
     line_items: [{ price: priceId, quantity: 1 }],
     customer_email: email,
     customer_creation: "always",
