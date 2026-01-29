64d55d0362dc4c2386339b95b8ff1cc198c10182
Afrie Irham <afrie@chip-in.asia>
2026-01-29 13:26:04 +0800

    add analytics

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -36,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script defer src="https://analytics.afrieirham.com/script.js" data-website-id="3de8ed05-3bf8-46fd-873f-f368586ad776"></script>
         <Meta />
         <Links />
       </head>
