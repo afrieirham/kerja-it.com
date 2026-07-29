ac34ace81937e0cb874b92f6e1f2c6782035dac0
Afrie Irham <afrie@chip-in.asia>
2026-07-29 19:11:32 +0800

    remove logo button padding

--- web/app/components/widget/header.tsx
+++ web/app/components/widget/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
 		<div className="bg-white">
 			<div className="container mx-auto flex w-full items-center justify-between border-b py-2">
 				<div>
-					<Button variant="link" size="xs" className="font-bold">
+					<Button variant="link" size="xs" className="font-bold px-0">
 						<Link to={href("/")}>Kerja-IT.com</Link>
 					</Button>
 				</div>
